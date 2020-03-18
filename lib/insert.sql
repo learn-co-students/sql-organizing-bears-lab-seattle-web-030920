57eca7b1bae197477f61f5b09c4668ab3ea46dc0
Your Name <you@example.com>
2020-03-18 13:54:54 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 15, 'M', 'Brown', 'Happy', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 20, 'M', 'Brown', 'Aggressive', 1);
+INSERT INTO bears VALUES (3, 'Tabitha', 17, 'F', 'Brown', 'Happy', 1);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 18, 'M', 'Brown', 'Happy', 1);
+INSERT INTO bears VALUES (5, 'Melissa', 14, 'M', 'Black', 'Happy', 1);
+INSERT INTO bears VALUES (6, 'Grinch', 22, 'M', 'Black', 'Aggressive', 1);
+INSERT INTO bears VALUES (7, 'Wendy', 18, 'F', 'Brown', 'Happy', 1);
+INSERT INTO bears VALUES (8, null, 15, 'M', 'Brown', 'Aggressive', 1);
+
+
