a536f12c0cd87b425943ced64554be1e5d87ac57
christine <bumpouce@gmail.com>
2020-03-17 22:10:22 -0700

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,20 @@
+CREATE TABLE bears (
+
+    id  INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    gender TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive INTEGER  
+
+);
+
+-- column	type
+-- id	integer
+-- name	text
+-- age	integer
+-- gender	text
+-- color	text
+-- temperament	text
+-- alive	boolean
