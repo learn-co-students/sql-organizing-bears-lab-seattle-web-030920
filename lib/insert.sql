3dda7b4bcc5f9d41b6b6a1f55731d41678e913d0
Aman <aman.david.michael@gmail.com>
2020-03-23 10:59:58 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name) Values("Mr. Chocolate");
+INSERT INTO bears(name) Values("Rowdy");
+INSERT INTO bears(name) Values("Tabitha");
+INSERT INTO bears(name) Values("Sergeant Brown");
+INSERT INTO bears(name) Values("Melissa");
+INSERT INTO bears(name) Values("Grinch");
+INSERT INTO bears(name) Values("Wendy");
+INSERT INTO bears(name) Values(NULL);
